97615e868a663bb0b51b3eeeedaf28486e278370
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2022-11-16 19:30:05 -0500

    Better cli ui

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from models import *
 from data_collections import *
 
 if __name__ == "__main__":
-    path = 'cran.all.1400'
+    # Create a welcome message
+    print("Welcome to the SRI")
+    print("=================================")
+    path = 'corpus/cran/cran.all.1400'
     print('Loading data...')
     with open(path, 'r', errors='ignore') as f:
         docs = CranCollection().parse(f)
@@ -10,10 +13,13 @@ if __name__ == "__main__":
 
     while True:
         query = input("Enter your query: ")
+        if query == 'q':
+            break
         print()
         ranking = vec.ranking(query, 5)
         for i, doc in enumerate(ranking):
             print("Rank: ", i + 1)
             print("Doc ID: ", doc.id)
             print("Title: ", doc.title)
+            print("Corpus: ", doc.corpus)
             print()
