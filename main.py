acf2cecee3e1bd1bc58c7918753cd16e9c90c166
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2022-12-16 20:24:49 -0500

    Create a main module

---
+++ main.py
@@ -0,0 +1,24 @@
+from models import *
+from data_collections import *
+
+if __name__ == "__main__":
+    # Create a welcome message
+    print("Welcome to the SRI")
+    print("=================================")
+    path = 'corpus/cran/cran.all.1400'
+    print('Loading data...')
+    docs = CranCollection().parse()
+    vec = BooleanExtended(docs)
+
+    while True:
+        query = input("Enter your query: ")
+        if query == 'q':
+            break
+        print()
+        ranking = vec.ranking(query, 5)
+        for i, doc in enumerate(ranking):
+            print("Rank: ", i + 1)
+            print("Doc ID: ", doc.id)
+            print("Title: ", doc.title)
+            print("Corpus: ", doc.corpus)
+            print()
