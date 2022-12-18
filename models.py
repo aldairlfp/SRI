7e48bcc5ca56c47d136eaccab356b24cc8ab3077
Aldair Alfonso <aldairalfonsoperez@gmail.com>
2022-12-18 02:54:11 -0500

    Implement rocchio algorithm

--- models.py
+++ models.py
@@ -7,7 +7,12 @@ from data_collections import default_processor
 import utils
 
 
-class VectorSpace(object):
+class Model(object):
+    def calculate_weights_query(self, query):
+        raise NotImplementedError
+
+
+class VectorSpace(Model):
     def __init__(self, docs, corpus_type):
         M = len(docs)  # number of files in dataset
         self.docs = docs
@@ -18,7 +23,7 @@ class VectorSpace(object):
         self._tf_idf = utils.tfidf(self.docs, self._tf_dict, self._idf_dict)  # returns tf-idf scores
         self.a = 0.5
 
-    def ranking(self, query, top):
+    def calculate_weights_query(self, query):
         query = default_processor(query, 'english')
         # create counter for query
         query_counter = Counter(query)
@@ -37,6 +42,11 @@ class VectorSpace(object):
         for word in query_tfidf:
             query_tfidf[word] = (self.a + (1 - self.a) * query_tfidf[word] / max_freq) * self._idf_dict[word]
 
+        return query_tfidf
+
+    def ranking(self, query):
+        query_tfidf = self.calculate_weights_query(query)
+
         scores = [0 for doc in self.docs]
         for i, doc in enumerate(self.docs):
             for word in query_tfidf:
@@ -47,6 +57,31 @@ class VectorSpace(object):
         scores_index = np.array(scores)
         scores_index = scores_index.argsort()[:][::-1]
         return [self.docs[i] for i in scores_index if scores[i] > 0]
+
+    def rocchio(self, query, alpha, beta, gamma, cr, cnr):
+        qm = self.calculate_weights_query(query)
+
+        for word in qm:
+            qm[word] *= alpha
+
+        for doc in cr:
+            doc_tfidf = self._tf_idf[doc.id]
+            for word in qm:
+                if word in doc_tfidf:
+                    qm[word] += beta * doc_tfidf[word] / len(cr)
+                else:
+                    qm[word] = doc_tfidf[word]
+
+        for doc in cnr:
+            doc_tfidf = self._tf_idf[doc.id]
+            for word in qm:
+                if word in doc_tfidf:
+                    qm[word] -= gamma * doc_tfidf[word] / len(cnr)
+                else:
+                    qm[word] = doc_tfidf[word]
+
+        return qm
+
 
     def __str__(self):
         return "vector_space" + '_' + self._corpus_type
