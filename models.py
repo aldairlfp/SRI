3a9b56fbbfda686df275e1120e29c1481401bfb0
Mauro-Bolado <maurovizoso@gmail.com>
2022-12-20 05:08:01 -0500

    similarity updated

--- models.py
+++ models.py
@@ -184,7 +184,7 @@ class Probabilistic(object):
                 p_i, r_i = self.get_relevance(document_id, common_term)
                 similarity += math.log((p_i * (1 - r_i)) / (r_i * (1 - p_i)))
 
-        return 0 if similarity == 0 else math.log(similarity)
+        return similarity
 
     def get_ranking(self, query, top, lang = 'english'):
         """Get the ranking of a query
@@ -213,7 +213,7 @@ class Probabilistic(object):
             doc_vector = self.document_vectors[index]
             sim = self.similarity(query_vector, doc_vector, index)
             
-            if sim > 0.5:
+            if sim > 1:
                 doc_rank.append((sim, index))
         self.last_ranking = sorted(
             doc_rank, key=lambda rank_index: rank_index[0], reverse=True)
